92f70e674f4c7e14adca66ab76760b7cc501d762
Ben S <89033552+benspak@users.noreply.github.com>
2025-03-28 08:43:31 -0600

    [Removed] Depricated code

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // Models
