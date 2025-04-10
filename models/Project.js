5b72ceda3d0293eab372278974f7c8d484238cae
Ben S <89033552+benspak@users.noreply.github.com>
2025-04-10 11:08:53 -0600

    [Up] userId of the person creating the project

--- models/Project.js
+++ models/Project.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const projectSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
   description: { type: String },
   goalIds: [{ type: Schema.Types.ObjectId, ref: "Response" }],
