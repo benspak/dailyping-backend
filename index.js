968d68aea6ebd60d910c2a4d4c15b1e892c9ad7d
Ben S <89033552+benspak@users.noreply.github.com>
2025-04-11 11:28:38 -0600

    [Up] moved to prodution cors

--- index.js
+++ index.js
@@ -67,20 +67,20 @@ app.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req, r
 const allowedOrigins = ['https://dailyping.org', 'https://stripe.com'];
 
 // Production
-// app.use(cors({
-//  origin: function (origin, callback) {
-//    if (!origin || allowedOrigins.includes(origin)) {
-//      callback(null, true);
-//    } else {
-//      callback(new Error("Not allowed by CORS"));
-//    }
-//  },
-//  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//  allowedHeaders: ["Content-Type", "Authorization"]
-// }));
+ app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+ }));
 
 // Allow all, only for development
-app.use(cors()); // ⚠️ for development only
+// app.use(cors());
 
 app.use(bodyParser.json());
 
