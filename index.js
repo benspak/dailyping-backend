40afabd3a4b5fc735c378b10c8d5af68e46abb2e
Ben S <89033552+benspak@users.noreply.github.com>
2025-04-12 17:30:26 -0600

    [Up] added dueDate

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ app.post('/billing/create-checkout-session', authenticateToken, async (req, res)
 
 app.post('/api/goal', authenticateToken, async (req, res) => {
   const userId = req.user.id;
-  const { content, mode, subTasks = [], reminders = [], note } = req.body;
+  const { content, mode, subTasks = [], reminders = [], note, dueDate } = req.body;
   const todayISO = new Date().toISOString().split('T')[0];
 
   try {
@@ -316,6 +316,7 @@ app.post('/api/goal', authenticateToken, async (req, res) => {
       content,
       mode,
       date: todayISO,
+      dueDate,
       reminders: user.pro === 'active' ? reminders : [],
       subTasks: cleanedSubTasks,
       createdAt: new Date(),
@@ -352,7 +353,7 @@ app.post('/api/goal', authenticateToken, async (req, res) => {
 // Update existing goal
 app.put('/api/goal/:id', authenticateToken, async (req, res) => {
   try {
-    const { content, reminders = [], subTasks = [], note } = req.body;
+    const { content, reminders = [], subTasks = [], note, dueDate } = req.body;
 
     const updated = await Goal.findById(req.params.id);
     if (!updated) return res.status(404).json({ error: 'Goal not found.' });
@@ -370,6 +371,7 @@ app.put('/api/goal/:id', authenticateToken, async (req, res) => {
     })).filter(t => t.text);
 
     updated.edited = true;
+    updated.dueDate = dueDate;
     updated.note = note;
 
     await updated.save();
